6602d3501f518244b8efb9b46b91f2a538a43fcc
Julia Morais <eijuliamorais@gmail.com>
2026-06-18 20:29:59 -0300

    feat: integra API e cria listagem de alunos

---
+++ frontend/src/pages/Alunos.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { onMount } from "svelte";
+  import api from "../services/api";
+
+  let alunos = [];
+
+  onMount(async () => {
+    try {
+      const response = await api.get("/alunos/");
+      alunos = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  });
+</script>
+
+<h1>Lista de Alunos</h1>
+
+<table border="1">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Nome</th>
+      <th>Matrícula</th>
+      <th>Curso</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    {#each alunos as aluno}
+      <tr>
+        <td>{aluno.id}</td>
+        <td>{aluno.nome}</td>
+        <td>{aluno.matricula}</td>
+        <td>{aluno.curso}</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
